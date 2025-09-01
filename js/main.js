10ac3b316d06723e7fbf5525e42eacab70baa176
Jefferson David Cano Hernandez <71351421+YefersonD-art@users.noreply.github.com>
2025-09-01 13:43:45 -0500

    Refactor HTML structure and improve formatting across multiple pages

--- js/main.js
+++ js/main.js
@@ -1,11 +1,8 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const toggle = document.querySelector(".menu-toggle");
-  const nav = document.querySelector(".nav-principal");
+document.addEventListener('DOMContentLoaded', () => {
+  const toggle = document.querySelector('.menu-toggle')
+  const nav = document.querySelector('.nav-principal')
 
-  toggle.addEventListener("click", () => {
-    nav.classList.toggle("show");
-  });
-});
-
-
-
+  toggle.addEventListener('click', () => {
+    nav.classList.toggle('show')
+  })
+})
